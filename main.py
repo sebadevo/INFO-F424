7cd83c911c6ab1141080096eaaad0f868269bae5
sebadevo <sebastien.de.vos@ulb.be>
2022-03-08 16:45:42 +0100

    fin de la partie 1

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-from __future__ import division
-from pulp import GLPK
 import pyomo.environ as pyo
 
+""" 1. CRÉATION DU MODÈLE """
+
+
 model = pyo.AbstractModel()
-""" 1. DÉCLARATION DES CONSTANTES, DES LISTES ET DICTIONNAIRES """
 
-model.I = pyo.Set(within=pyo.NonNegativeIntegers)
-model.size = pyo.Param()
+
+""" 2. DÉCLARATION DES CONSTANTES, DES LISTES ET DICTIONNAIRES """
+
+
+model.I = pyo.Set()
 model.cap = pyo.Param()
+model.size = pyo.Param(model.I, within=pyo.NonNegativeIntegers)
 
-tmps = 1  # number of minutes
+model.p = pyo.Set(initialize=model.I)
+model.b = pyo.Set(initialize=model.I)
 
-""" 2. CRÉATION DU MODÈLE """
+time = 10  # number of minutes the program can run
 
-model.p = pyo.RangeSet(1, model.I)
-model.b = pyo.RangeSet(1, model.I)
-
-# model = LpProblem(name="Bin Packing problem", sense=LpMinimize)
 
 """ 3. DÉCLARATION DES VARIABLES """
+
 
 model.y = pyo.Var(model.b, domain=pyo.Binary)  # 1 if box b is used
 model.x = pyo.Var(model.p, model.b, domain=pyo.Binary)  # 1 if product p is in box b
 
 
-# Objective function definition
+""" 4. DÉCLARATION DE LA FONCTION OBJECTIVE """
+
 
 def obj_expression(m):
     return pyo.summation(m.y)
@@ -33,8 +36,11 @@ def obj_expression(m):
 model.OBJ = pyo.Objective(rule=obj_expression)
 
 
+""" 4. DÉCLARATION DES CONTRAINTES """
+
+
 def xcy_constraint_rule(m, b):
-    return sum(m.x[p, b] for p in m.p) <= m.cap * m.y[b]
+    return sum(m.x[p, b] * m.size[p] for p in m.p) <= m.cap * m.y[b]
 
 
 model.XcyConstraint = pyo.Constraint(model.b, rule=xcy_constraint_rule)
@@ -47,14 +53,4 @@ def x_constraint_rule(m, p):
 model.XConstraint = pyo.Constraint(model.p, rule=x_constraint_rule)
 
 
-""" 3. DÉCLARATION DES VARIABLES """
-# x = LpVariable.dicts("BagFiledWith", [(p, b) for p in range(1, P + 1) for b in range(1, B + 1)], 0, 1, cat=LpBinary)
-
-# y = LpVariable.dicts("BagFiledWith", [p for p in range(1, P + 1)], 0, 1, cat=LpBinary)
-
-""" 4. DÉCLARATION DES CONTRAINTES """
-
-""" 5. DÉCLARATION DE LA FONCTION OBJECTIVE """
-
-# status = model.solve(solver=GLPK(msg=True, keepFiles=True, options=["--tmlim", str(60 * tmps)]))
 """ 6. RÉCUPÉRATION DES RÉSULTATS """
